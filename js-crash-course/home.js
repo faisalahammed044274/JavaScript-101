de1198168666b690e0699412abb767899dad3244
Faisal Ahammed <50483833+faisalahammed044274@users.noreply.github.com>
2024-08-02 13:40:33 +0600

    increment and decrement

--- js-crash-course/home.js
+++ js-crash-course/home.js
@@ -1,3 +1,26 @@
-console.log('Hey This is Faisal');
-alert('WOW! JavaScript is awesome.');
-console.log('Tester Tested');
+console.log('Hey,This is Faisal.');
+// alert('WOW! JavaScript is awesome.');
+
+//Variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var height = 5.6;
+console.log(height);
+
+document.getElementById('someText').innerHTML = 'Hello World!';
+
+prompt("What is your age ?");
+
+// increment
+number1 = 10;
+number1++;
+number1++;
+console.log(number1);
+// decrement
+number1--;
+console.log(number1);
+
